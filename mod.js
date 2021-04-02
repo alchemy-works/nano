fc2d476e689ebe3df3fc881a0705bc61c1ea14cd
Cbdy <cbdyzj@jianzhao.org>
2021-04-02 23:02:14 +0800

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,20 +1,15 @@
 function handleRequest(request) {
-    const { pathname } = new URL(request.url);
 
-    return new Response(
-        `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-      <h1>nano</h1>
-      <p>Hi there, this is <strong>nano</strong></p>
-    </body>`,
-        {
-            headers: {
-                "content-type": "text/html; charset=UTF-8",
-            },
+    const json = JSON.stringify({
+        group: 'alchemy-works',
+        name: 'nano',
+    })
+
+    return new Response(json, {
+        headers: {
+            'content-type': 'application/json; charset=UTF-8',
         },
-    )
+    })
 }
 
 addEventListener("fetch", (event) => {
